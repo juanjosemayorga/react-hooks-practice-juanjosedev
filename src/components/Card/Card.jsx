54952be358149b150b949f8e62173fbd26b06d1d
juanjosemayorga <tmaniacus@gmail.com>
2020-12-02 17:29:12 -0600

    card styled with scss

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import './card'
 
 export const Card = ({ image, name, status }) => {
   return (
-    <div className="CardContainer">
-      <img src={image} alt={name}/>
-      <h3>{name}</h3>
-      <p>{status}</p>
+    <div className="card">
+      <img className="card__image" src={image} alt={name}/>
+      <h3 className="card__name">{name}</h3>
+      <p className="card__status">{status}</p>
     </div>
   )
 }
